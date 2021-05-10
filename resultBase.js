aa0f8d5ecbc1b223d790912f15e25e0ccd6c690a
Olof Svahn <svahnolof@gmail.com>
2021-05-10 15:28:49 +0200

    Added image hover

--- resultBase.js
+++ resultBase.js
@@ -22,22 +22,37 @@ const lbSize = "b";
 
 const apikey = "b008fe0d80fecbe8e2dbdfe21b0aca32";
 
-console.log(apikey)
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+/*const maxItem = 10;
+const count = 0;
+let index = 1;*/
 
 async function FetchResults ()
 {
     const response = await fetch(`https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=${apikey}&text=${searchText}&per_page=${hits[hitsPerPage]}&sort=${sorts[sort]}&format=json&nojsoncallback=1`);
     const data = await response.json();
 
-    console.log(response);
-
     data.photos.photo.forEach(photo => {
         const server = photo.server;
         const id = photo.id;
         const secret = photo.secret;
         const thumbnail = document.createElement("img");
-        thumbnail.src = `https://live.staticflickr.com/${server}/${id}_${secret}_${tnSize}.jpg`
-        searchResult.appendChild(thumbnail)
+        thumbnail.src = `https://live.staticflickr.com/${server}/${id}_${secret}_${tnSize}.jpg`;
+        searchResult.appendChild(thumbnail);
+
+        /*if(count >= (index * maxItem)-maxItem && count < index * maxItem)
+        {
+            console.log("in")
+            thumbnail.classList.remove("hide");
+            thumbnail.classList.add("show");
+        }
+        else
+        {
+            thumbnail.classList.remove("show");
+            thumbnail.classList.add("hide");   
+        }
+        count++;*/
     });
 
     const allImages = document.querySelectorAll(".searchResult img",);
